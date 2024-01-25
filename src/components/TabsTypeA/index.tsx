1fa604a3cba5d567f39c498b3983641aa9f5cf65
vikrantaroraa <arora.veshu@gmail.com>
2024-01-25 20:46:28 +0530

    feat: created TabsTypeA and TabsTypeB components in components and added them to TabsTypeA.mdx and TabsTypeB.mdx files

---
+++ src/components/TabsTypeA/index.tsx
@@ -0,0 +1,40 @@
+import {
+  TabsTypeA,
+  TabsDataProps,
+  DummyComponent1,
+  DummyComponent3,
+} from "canary-design";
+
+const tabsData: TabsDataProps[] = [
+  {
+    key: "1",
+    label: "Tab 1",
+    children: <DummyComponent1 />,
+  },
+  {
+    key: "2",
+    label: "Tab 2",
+    children: "Tab2 content is shown here.",
+  },
+  {
+    key: "3",
+    label: "Tab 3",
+    children: <DummyComponent3 />,
+  },
+];
+
+const ExampleTabsTypeA = () => {
+  const tabHandler = (index: number) => {
+    console.log(`active tab index: ${index}`);
+  };
+
+  return (
+    <TabsTypeA
+      tabsData={tabsData}
+      tabHandler={tabHandler}
+      defaultActiveKey={1}
+    />
+  );
+};
+
+export { ExampleTabsTypeA };
