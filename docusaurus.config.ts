58efc45f0df9314afec083a3a69a9c121998abe9
vikrantaroraa <arora.veshu@gmail.com>
2024-01-29 02:02:45 +0530

    fix: fixed broken link in footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/docs/category/getting-started",
             },
           ],
         },
